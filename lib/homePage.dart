150c60d0fd9408235cf715146fc88a020c7d335c
hawkeye4567 <adarshtheking29@gmail.com>
2021-04-19 11:46:40 +0530

    added bottom navigation bar and other pages added

---
+++ lib/homePage.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'search.dart';
+import 'library.dart';
+import 'premium.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int index = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: show(index),
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          canvasColor: Colors.grey[900],
+        ),
+        child: BottomNavigationBar(
+          currentIndex: index,
+          showUnselectedLabels: true,
+          unselectedItemColor: Colors.white54,
+          selectedItemColor: Colors.white,
+          onTap: ((int x) {
+            setState(() {
+              index = x;
+            });
+          }),
+          items: [
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text("Home"),
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text("Search"),
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(Icons.library_music),
+              title: Text("Your library"),
+            ),
+            new BottomNavigationBarItem(
+              icon: Icon(FontAwesomeIcons.spotify),
+              title: Text("Premium"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget show(int index) {
+  switch (index) {
+    case 0:
+      return HomePage();
+      break;
+    case 1:
+      return Search();
+      break;
+    case 2:
+      return Library();
+      break;
+    case 3:
+      return Premium();
+      break;
+  }
+}
---
+++ lib/premium.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Premium extends StatefulWidget {
+  @override
+  _PremiumState createState() => _PremiumState();
+}
+
+class _PremiumState extends State<Premium> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Get Premium"),
+    );
+  }
+}
